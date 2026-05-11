d1549763be6663f31be0ab544c36099d5700c9cc
Eran Boodnero <eboodnero@gmail.com>
2026-05-11 05:23:32 -0700

    chore: align tauri dev server port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const host = process.env.TAURI_DEV_HOST
 export default defineConfig({
   clearScreen: false,
   server: {
-    port: 5173,
+    port: 4173,
     strictPort: true,
     host: host || false,
     hmr: host
